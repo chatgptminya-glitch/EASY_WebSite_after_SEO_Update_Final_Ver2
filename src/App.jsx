f8eca4696d9cc477db8d1638a61fd5bdd3497b67
chatgptminya-glitch <chatgpt.minya@gmail.com>
2025-09-22 01:22:50 +0300

    Update App.jsx
    editing social media links Mon 22 Sep 1:22AM

--- src/App.jsx
+++ src/App.jsx
@@ -399,15 +399,15 @@ ${firstName} ${lastName}`
                 
                 {/* Social Media Links */}
                 <div className="flex justify-center space-x-6 mb-8">
-                  <a href="https://facebook.com/easyparking.smartvillage" target="_blank" rel="noopener noreferrer" className="text-white hover:text-yellow-300 transition-colors">
+                  <a href="https://www.facebook.com/groups/774443268515072/?ref=share&mibextid=KtfwRi" target="_blank" rel="noopener noreferrer" className="text-white hover:text-yellow-300 transition-colors">
                     <span className="sr-only">Facebook</span>
                     <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24"><path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/></svg>
                   </a>
-                  <a href="https://youtube.com/@easyparking.smartvillage" target="_blank" rel="noopener noreferrer" className="text-white hover:text-red-400 transition-colors">
+                  <a href="https://www.youtube.com/watch?v=3X-rvMM2qME" target="_blank" rel="noopener noreferrer" className="text-white hover:text-red-400 transition-colors">
                     <span className="sr-only">YouTube</span>
                     <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24"><path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/></svg>
                   </a>
-                  <a href="https://instagram.com/easyparking.smartvillage" target="_blank" rel="noopener noreferrer" className="text-white hover:text-pink-400 transition-colors">
+                  <a href="https://www.instagram.com/easy_smartvillage/?igsh=MTEwOWIzaTNhbHBiNA%3D%3D&utm_source=qr" target="_blank" rel="noopener noreferrer" className="text-white hover:text-pink-400 transition-colors">
                     <span className="sr-only">Instagram</span>
                     <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24"><path d="M12.017 0C5.396 0 .029 5.367.029 11.987c0 6.62 5.367 11.987 11.988 11.987c6.62 0 11.987-5.367 11.987-11.987C24.014 5.367 18.637.001 12.017.001zM8.449 16.988c-1.297 0-2.448-.49-3.323-1.297C4.198 14.895 3.708 13.744 3.708 12.447s.49-2.448 1.297-3.323C5.902 8.198 7.053 7.708 8.35 7.708s2.448.49 3.323 1.297c.897.875 1.387 2.026 1.387 3.323s-.49 2.448-1.297 3.323c-.875.897-2.026 1.387-3.323 1.387zm7.718 0c-1.297 0-2.448-.49-3.323-1.297-.897-.875-1.387-2.026-1.387-3.323s.49-2.448 1.297-3.323c.875-.897 2.026-1.387 3.323-1.387s2.448.49 3.323 1.297c.897.875 1.387 2.026 1.387 3.323s-.49 2.448-1.297 3.323c-.875.897-2.026 1.387-3.323 1.387z"/></svg>
                   </a>
@@ -931,9 +931,9 @@ ${firstName} ${lastName}`
             <div>
               <h4 className="font-semibold mb-4">Follow Us</h4>
               <ul className="space-y-2 text-gray-400">
-                <li><a href="https://facebook.com/easyparking.smartvillage" target="_blank" rel="noopener noreferrer" className="hover:text-white transition-colors">Facebook</a></li>
-                <li><a href="https://youtube.com/@easyparking.smartvillage" target="_blank" rel="noopener noreferrer" className="hover:text-white transition-colors">YouTube</a></li>
-                <li><a href="https://instagram.com/easyparking.smartvillage" target="_blank" rel="noopener noreferrer" className="hover:text-white transition-colors">Instagram</a></li>
+                <li><a href="https://www.facebook.com/groups/774443268515072/?ref=share&mibextid=KtfwRi" target="_blank" rel="noopener noreferrer" className="hover:text-white transition-colors">Facebook</a></li>
+                <li><a href="https://www.youtube.com/watch?v=3X-rvMM2qME" target="_blank" rel="noopener noreferrer" className="hover:text-white transition-colors">YouTube</a></li>
+                <li><a href="https://www.instagram.com/easy_smartvillage/?igsh=MTEwOWIzaTNhbHBiNA%3D%3D&utm_source=qr" target="_blank" rel="noopener noreferrer" className="hover:text-white transition-colors">Instagram</a></li>
               </ul>
             </div>
             
